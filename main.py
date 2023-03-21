8409393ecf40a98f2baac6fa67697135d06eaf4e
lusiluvitasari <128022033+lusiluvitasari@users.noreply.github.com>
2023-03-21 09:54:01 +0000

    Update Biodata

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Lusi Luvita Sari</p>"
+  return "<p>Nama : Lusi Luvita Sari</p><p> Tempat,Tanggal Lahir : Malang, 11 Juli 2003</p><p> Alamat : Dusun.Purwoasri, Singosari, Malang</p><p> Jenis Kelamin : Perempuan</p><p> Kewarganegaraan : Indonesia</p><p> Agama : Islam</p>"
 
 
 if __name__ == '__main__':
